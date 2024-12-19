60f368ffef089c2b14c481180932625b91299e69
Shahidullah-Kaisar <shkaisar2002@gmail.com>
2024-12-19 21:33:18 +0600

    searchbar

--- frontend/src/layouts/Root.jsx
+++ frontend/src/layouts/Root.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Outlet } from 'react-router-dom';
 import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
+import SearchBar from '../components/SearchBar';
 
 
 const Root = () => {
@@ -9,6 +10,7 @@ const Root = () => {
         <>
             <div className='px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]'>
                 <Navbar></Navbar>
+                <SearchBar></SearchBar>
                 <Outlet></Outlet>
                 <Footer></Footer> 
             </div>
