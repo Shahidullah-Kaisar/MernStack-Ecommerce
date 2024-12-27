8b96fd0f26abe93934c4b4bf21b9e46fe5e5e6fb
Shahidullah-Kaisar <shkaisar2002@gmail.com>
2024-12-27 18:41:24 +0600

    cartTotal Update

--- frontend/src/pages/Cart.jsx
+++ frontend/src/pages/Cart.jsx
@@ -2,6 +2,7 @@ import React, { useContext, useEffect, useState } from 'react';
 import { ShopContext } from '../context/ShopContextProvider';
 import Title from '../components/Title';
 import { assets } from '../assets/assets';
+import CartTotal from '../components/CartTotal';
 
 const Cart = () => {
 
@@ -61,6 +62,13 @@ const Cart = () => {
                     
                 }
             </div>
+
+            <div className='flex justify-end my-20'>
+                <div className='w-full sm:w-[450px]'>
+                    <CartTotal></CartTotal>
+                </div>
+            </div>
+
         </div>
     );
 };
