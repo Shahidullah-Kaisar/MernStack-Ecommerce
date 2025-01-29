8d61c2cd212916452fe07eaee7f2dc1caacde95f
Shahidullah-Kaisar <shkaisar2002@gmail.com>
2025-01-29 15:06:03 +0600

    placeorder

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -74,23 +74,3 @@ export default Navbar;
 
 
 
-{/* <header className='bg-green-950 fixed w-full top-0 left-0 z-50'>
-                <nav className='container flex items-center justify-between h-16 sm:h-20'>
-                    <div className='font-Arima sm:text-2xl'>eStudy</div>
-
-                    <div className={`absolute top-0 ${menuOpen ? 'left-0' : 'left-[-100%]'} min-h-[80vh] w-full bg-green-950/80 backdrop-blur-sm flex items-center justify-center duration-300 overflow-hidden lg:static lg:min-h-fit lg:bg-transparent lg:w-auto`}>
-                        <ul className='flex flex-col items-center gap-8 lg:flex-row'>
-
-                            {navLinks}
-                            
-                        </ul>
-                        
-                        <div className='absolute top-5 right-5 text-3xl cursor-pointer lg:hidden' onClick={handleMenu}>
-                            <IoMdClose/>
-                        </div>
-                    </div>
-                    <div className='text-xl sm:text-3xl cursor-pointer x-50 lg:hidden' onClick={handleMenu} >
-                        <IoMdMenu />
-                    </div>
-                </nav>
-            </header> */}
