49b8d383f705a85de1d28883bc394d6f2eb24c51
Shahidullah-Kaisar <shkaisar2002@gmail.com>
2025-02-03 12:04:05 +0600

    login

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,10 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Login = () => {
+    const [currentState, setCurrentState] = useState('Sign Up')
     return (
-        <div>
-            <h2>login</h2>
-        </div>
+        <form>
+            <div>
+                <p>{currentState}</p>
+                <hr />
+            </div>
+            { currentState === 'Login' ? " " : <input type="text" placeholder='Name'/> }
+            <input type="email" placeholder='Email'/>
+            <input type="password" placeholder='Password'/>
+            <div>
+                <p>Forgot your password?</p>
+                {
+                    currentState === "Login"
+                    ? <p onClick={()=>setCurrentState('Sign Up')} className='cursor-pointer'>Create a account</p>
+                    : <p onClick={()=>setCurrentState('Login')} className='cursor-pointer'>Login</p>
+                }
+            </div>
+        </form>
     );
 };
 
